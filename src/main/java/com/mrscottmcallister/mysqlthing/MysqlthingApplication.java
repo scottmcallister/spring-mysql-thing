d3bed9aaf9999acea54c4b0b3afcbe1879224ade
Scott McAllister <scottm@nextopia.com>
2016-09-21 20:47:20 -0400

    first commit

---
+++ src/main/java/com/mrscottmcallister/mysqlthing/MysqlthingApplication.java
@@ -0,0 +1,12 @@
+package com.mrscottmcallister.mysqlthing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlthingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlthingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mrscottmcallister/mysqlthing/MysqlthingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mrscottmcallister.mysqlthing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MysqlthingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
