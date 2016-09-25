dbed6e83020cc96c3bd269388380ef935659bf50
Scott McAllister <scottm@nextopia.com>
2016-09-25 16:05:32 -0400

    move Thing.java to model directory

--- src/main/java/com/mrscottmcallister/mysqlthing/models/Thing.java
+++ src/main/java/com/mrscottmcallister/mysqlthing/models/Thing.java
@@ -1,8 +1,25 @@
-package com.mrscottmcallister.mysqlthing;
+package com.mrscottmcallister.mysqlthing.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.*;
+
+@Table(appliesTo = "things")
 public class Thing {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@NotNull
 	private String name;
+	
+	@NotNull
 	private String description;
+	
 	public Thing(String pName, String pDescription){
 		name = pName;
 		description = pDescription;
